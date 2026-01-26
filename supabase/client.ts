e270a6b2772b34a48ef6a9b95e32889c85885893
Jonathan McEwen <109186293+TherealJonWolf@users.noreply.github.com>
2026-01-25 20:37:50 -0600

    Update client.ts

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,19 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Load environment variables
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabase_Service_Role_Key = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-// Check environment variables
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables");
+  throw new Error("Missing frontend Supabase env vars");
 }
 
-// Only log in development mode
-if (import.meta.env.DEV) {
-  console.log("Supabase URL:", supabaseUrl);
-  console.log("Supabase Key is set:", !!supabaseAnonKey); // Don't log the key itself
-}
-
-// Create the Supabase client
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
